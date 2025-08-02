844f172e3bbd95d594055613cd57989bc7ccd4e3
Ankit Thakur <ankitthakur1430@gmail.com>
2025-08-03 01:46:37 +0530

    Correct one code

--- Array's/CheckSorting.java
+++ Array's/CheckSorting.java
@@ -1,12 +1,21 @@
 public class CheckSorting {
     public static void main(String[] args) {
         int[] arr = {1,2,1,3,4};
+        boolean isSorted = true;
         for(int i = 1 ; i<arr.length;i++)
         {
-            if(arr[i]>arr[i-1])
+            if(arr[i]<arr[i-1])
             {
-                System.out.println("Array is Sorted");
+                isSorted = false;
+                break;
             }
+        }
+        if(isSorted)
+        {
+            System.out.println("Array is sorted");
+        }
+        else{
+        System.out.println("Array is not Sorted");
         }
     }
 }
